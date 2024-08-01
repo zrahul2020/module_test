35e872ba52c7068717609466daa039df98eaf92c
Rohit <rohit2020@gmail.com>
2024-08-01 13:29:30 -0700

    Patch 1

--- geometry/cube.go
+++ geometry/cube.go
@@ -1,5 +1,12 @@
 package geometry
 
-func CubeVolume(n int) int {
-	return n * n * n
+import "errors"
+
+func CubeVolume(n int) (int, error) {
+	if n > 0 {
+		return n * n * n, nil
+	} else {
+		return 0, errors.New("side length should be greater that zero")
+	}
+
 }
